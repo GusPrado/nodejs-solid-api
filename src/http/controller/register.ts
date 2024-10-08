195c31858bb9fcc3a7e0d2745154b341c88e627d
Gus Prado <49629739+GusPrado@users.noreply.github.com>
2024-10-07 21:57:48 -0300

    feat: Add global error handler

--- src/http/controller/register.ts
+++ src/http/controller/register.ts
@@ -2,6 +2,7 @@ import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
 import { RegisterUseCase } from '@/use-cases/register'
 import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
+import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-error'
 
 export async function register(req: FastifyRequest, res: FastifyReply) {
   const registerBodySchema = z.object({
@@ -21,7 +22,10 @@ export async function register(req: FastifyRequest, res: FastifyReply) {
       password,
     })
   } catch (err) {
-    return res.status(409).send()
+    if (err instanceof UserAlreadyExistsError) {
+      return res.status(409).send({ message: err.message })
+    }
+    throw err
   }
 
   return res.status(201).send()
